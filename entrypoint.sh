6c62da926a8c760970328470781298bb16e84c28
Sergey Nazarov <natarajaya@gmail.com>
2019-11-18 18:24:20 -0800

    Handle signals properly in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,27 +15,41 @@ SSL_CHECK_COMMAND="./ssl-cert-check"
 if [ "${TARGET_FILE}" != "" ]; then
   SSL_CHECK_COMMAND="${SSL_CHECK_COMMAND} -f ${TARGET_FILE} -x ${RENEWAL_WINDOW} -P"
 else
-	SSL_CHECK_COMMAND="${SSL_CHECK_COMMAND} -s ${TARGET_HOST} -p ${TARGET_PORT} -x ${RENEWAL_WINDOW} -P"
+  SSL_CHECK_COMMAND="${SSL_CHECK_COMMAND} -s ${TARGET_HOST} -p ${TARGET_PORT} -x ${RENEWAL_WINDOW} -P"
 fi
 
 ssl_check() {
-	echo -ne "$(${SSL_CHECK_COMMAND})"
+  echo -ne "$(${SSL_CHECK_COMMAND})"
 }
+
+signal_handler()
+{
+  kill -s SIGINT $!
+  exit 0
+}
+
+trap signal_handler SIGINT SIGTERM
 
 sleep "${SLEEP}"
 
 ssl_check_data="$(ssl_check)"
 
 if [ "${DAEMON}" == "true" ]; then
-	while { echo -en "HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n${ssl_check_data}\n"; } | nc -l -p "${DAEMON_PORT}"; do
-	  if [ "${DEBUG}" == "true" ]; then
-	  	echo "${ssl_check_data}"
-	  fi
+  {
+    while { echo -en "HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n${ssl_check_data}\n"; } | nc -l -p "${DAEMON_PORT}"; do
+      if [ "${DEBUG}" == "true" ]; then
+        echo "${ssl_check_data}"
+      fi
 
-	  echo
+      echo
 
-	  ssl_check_data="$(ssl_check)"
-	done
+      ssl_check_data="$(ssl_check)"
+    done
+  } &
+
+  while true; do
+    wait $!
+  done
 else
-	echo "${ssl_check_data}"
+  echo "${ssl_check_data}"
 fi

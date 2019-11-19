48e82cc576cd3c32e916356ebcccc009cf9c6c33
Sergey Nazarov <natarajaya@gmail.com>
2019-11-18 17:14:54 -0800

    Add TARGET_FILE option

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,23 @@
 DEBUG=${DEBUG:-"false"}
 SLEEP=${SLEEP:-"0"}
 
+TARGET_FILE=${TARGET_FILE:-""}
 TARGET_HOST=${TARGET_HOST:-"google.com"}
 TARGET_PORT=${TARGET_PORT:-"443"}
 RENEWAL_WINDOW=${RENEWAL_WINDOW:-"29"}
 DAEMON=${DAEMON:-"true"}
 DAEMON_PORT=${DAEMON_PORT:-"9402"}
 
+SSL_CHECK_COMMAND="./ssl-cert-check"
+
+if [ "${TARGET_FILE}" != "" ]; then
+  SSL_CHECK_COMMAND="${SSL_CHECK_COMMAND} -f ${TARGET_FILE} -x ${RENEWAL_WINDOW} -P"
+else
+	SSL_CHECK_COMMAND="${SSL_CHECK_COMMAND} -s ${TARGET_HOST} -p ${TARGET_PORT} -x ${RENEWAL_WINDOW} -P"
+fi
+
 ssl_check() {
-	echo -ne "$(./ssl-cert-check -s "${TARGET_HOST}" -p "${TARGET_PORT}" -x "${RENEWAL_WINDOW}" -P)"
+	echo -ne "$(${SSL_CHECK_COMMAND})"
 }
 
 sleep "${SLEEP}"
